9cd7ff897e4cc2d5a0880338c23eb9767257f1b3
Uday <jhariyauday60@gmail.com>
2026-02-15 23:39:16 +0530

    completed

--- xor_nn.cpp
+++ xor_nn.cpp
@@ -19,13 +19,17 @@ double sigmoid_derivative(double x) {
 
 class NeuralNetwork {
 private:
-    input input_size, hidden_size, output_size;
+    int input_size, hidden_size, output_size;
 
     vector<vector<double>> weights_input_hidden;
     vector<vector<double>> weights_hidden_output;
 
     vector<double> hidden;
-    vector<double> output:
+    vector<double> output;
+
+    vector<double> bias_hidden;
+    vector<double> bias_output;
+
 
 public:
     NeuralNetwork(int in, int hid, int out) : input_size(in), hidden_size(hid), output_size(out) {
@@ -33,32 +37,41 @@ public:
 
         // Initialize weights randomly
         weights_input_hidden.resize(input_size, vector<double>(hidden_size));
-        weights_hidden_output.resize(hidden_size, vector<double>>(output_size));
+        weights_hidden_output.resize(hidden_size, vector<double>(output_size));
 
         for (int i=0; i<input_size; i++)
             for (int j=0; j<hidden_size; j++) 
                 weights_input_hidden[i][j] = ((double)rand()/RAND_MAX);
         
-        for (int i=0; i<input_size; i++)
-            for (int j=0; j<hidden_size; j++) 
+        for (int i=0; i<hidden_size; i++)
+            for (int j=0; j<output_size; j++) 
                 weights_hidden_output[i][j] = ((double)rand()/RAND_MAX);
 
         hidden.resize(hidden_size);
         output.resize(output_size);
-    
+        
+        bias_hidden.resize(hidden_size);
+        bias_output.resize(output_size);
+        
+        for (int i = 0; i < hidden_size; i++)
+            bias_hidden[i] = ((double)rand()/RAND_MAX)*2-1;
+        
+        for (int i = 0; i < output_size; i++)
+            bias_output[i] = ((double)rand()/RAND_MAX)*2-1;     
+    }
 
     // forward propagation
 
     vector<double> forward(vector<double> input) {
         for (int i=0; i<hidden_size; i++) {
-            hidden[i] = 0.0;
+            hidden[i] = bias_hidden[i];
             for (int j=0; j<input_size; j++) 
                 hidden[i] += input[j]*weights_input_hidden[j][i];
             hidden[i] = sigmoid(hidden[i]);
         }
 
-        for (int i=0; i<output_size: i++) {
-            output[i] = 0.0;
+        for (int i=0; i<output_size; i++) {
+            output[i] = bias_output[i];
             for(int j=0; j<hidden_size; j++) 
                 output[i] += hidden[j]*weights_hidden_output[j][i];
             output[i] = sigmoid(output[i]);
@@ -89,11 +102,58 @@ public:
         // update weights hidden to output
         for (int i=0; i<hidden_size; i++)
             for (int j=0; j<output_size; j++) 
-                weights_hidden_output[i][j] += lr*output_errors[j]*sigmoid_derivative(output[j]*hidden[i]);
+                weights_hidden_output[i][j] += lr*output_errors[j]*sigmoid_derivative(output[j])*hidden[i];
 
+        for (int i=0; i<output_size; i++)
+        bias_output[i] += lr * output_errors[i] * sigmoid_derivative(output[i]);
+            
         // update weights input to hidden
         for (int i=0; i<input_size; i++)
             for (int j=0; j<hidden_size; j++)
-                weights_input_hidden[i][j] += lr*hidden_errors[j]*sigmoid_derivative(hidden[j]*input[i]);
+                weights_input_hidden[i][j] += lr*hidden_errors[j]*sigmoid_derivative(hidden[j])*input[i];
+        
+        for (int i=0; i<hidden_size; i++)
+        bias_hidden[i] += lr * hidden_errors[i] * sigmoid_derivative(hidden[i]);
     }
 };
+
+int main() {
+
+    NeuralNetwork nn(2, 2, 1);
+
+    vector<vector<double>> inputs = {
+        {0,0},
+        {0,1},
+        {1,0},
+        {1,1}
+    };
+
+    vector<vector<double>> targets = {
+        {0},
+        {1},
+        {1},
+        {0}
+    };
+
+    // training
+    for(int epoch = 0; epoch < 50000; epoch++) {
+        for(int i = 0; i < inputs.size(); i++) {
+            nn.train(inputs[i], targets[i], 0.5);
+        }
+    }
+
+    // testing
+    for(int i = 0; i < inputs.size(); i++) {
+        vector<double> result = nn.forward(inputs[i]);
+        double probability = result[0];
+        int predicted = (probability > 0.5) ? 1 : 0;
+
+        cout << inputs[i][0] << " XOR "
+                << inputs[i][1]
+                << " => raw: " << probability
+                << " | predicted: " << predicted
+                << endl;
+    }
+
+    return 0;
+}
