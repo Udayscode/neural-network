2d8f2f4e8e7136c7169cebba925a9ec5d826fda1
Uday <jhariyauday60@gmail.com>
2026-02-15 16:23:28 +0530

    implemented backpropogation

--- xor_nn.cpp
+++ xor_nn.cpp
@@ -45,25 +45,55 @@ public:
 
         hidden.resize(hidden_size);
         output.resize(output_size);
+    
+
+    // forward propagation
+
+    vector<double> forward(vector<double> input) {
+        for (int i=0; i<hidden_size; i++) {
+            hidden[i] = 0.0;
+            for (int j=0; j<input_size; j++) 
+                hidden[i] += input[j]*weights_input_hidden[j][i];
+            hidden[i] = sigmoid(hidden[i]);
+        }
+
+        for (int i=0; i<output_size: i++) {
+            output[i] = 0.0;
+            for(int j=0; j<hidden_size; j++) 
+                output[i] += hidden[j]*weights_hidden_output[j][i];
+            output[i] = sigmoid(output[i]);
+        }
+
+        return output;
     }
-}
 
-// forward propagation
+    // backpropagation
 
-vector<double> forward(vector<double> input) {
-    for (int i=0; i<hidden_size; i++) {
-        hidden[i] = 0.0;
-        for (int j=0; j<input_size; j++) 
-            hidden[i] += input[j]*weights_input_hidden[j][i];
-        hidden[i] = sigmoid(hidden[i]);
+    void train(vector<double> input, vector<double> target, double lr) {
+        forward(input);
+
+        vector<double> output_errors(output_size);
+        vector<double> hidden_errors(hidden_size);
+
+        // output layer error
+        for (int i=0; i<output_size; i++) 
+            output_errors[i] = target[i] - output[i];
+
+        // hidden layer error
+        for (int i=0; i<hidden_size; i++) {
+            hidden_errors[i] = 0;
+            for (int j=0; j<output_size; j++) 
+                hidden_errors[i] += output_errors[j]*weights_hidden_output[i][j];
+        }
+
+        // update weights hidden to output
+        for (int i=0; i<hidden_size; i++)
+            for (int j=0; j<output_size; j++) 
+                weights_hidden_output[i][j] += lr*output_errors[j]*sigmoid_derivative(output[j]*hidden[i]);
+
+        // update weights input to hidden
+        for (int i=0; i<input_size; i++)
+            for (int j=0; j<hidden_size; j++)
+                weights_input_hidden[i][j] += lr*hidden_errors[j]*sigmoid_derivative(hidden[j]*input[i]);
     }
-
-    for (int i=0; i<output_size: i++) {
-        output[i] = 0.0;
-        for(int j=0; j<hidden_size; j++) 
-            output[i] += hidden[j]*weights_hidden_output[j][i];
-        output[i] = sigmoid(output[i]);
-    }
-
-    return output;
-}
+};
